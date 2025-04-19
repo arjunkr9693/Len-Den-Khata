c6a214d39fe38d215e4fd6b3714527cda0515986
Arjun Sahoo <arjunkumar9693898775@gmail.com>
2025-04-19 19:11:25 +0530

    Added animated navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,6 +86,7 @@ dependencies {
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
+    implementation("androidx.navigation:navigation-compose:2.8.9")
 
     implementation (libs.gson)
 
